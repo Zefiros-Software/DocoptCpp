ee0c6281b8563b919120c6462b01a726461275b6
PaulVisscher <paul.pev@hotmail.com>
2018-02-26 22:57:24 +0100

    Update zpm 2 definition

--- script/appveyor-build.bat
+++
@@ -1,7 +0,0 @@
-cd test/
-premake5 install-package --allow-install --allow-module  || exit /b 1
-premake5 vs2015 || exit /b 1
-
-msbuild DocOpt.sln /property:Configuration=Test /property:Platform=Win32 || exit /b 1
-
-.\bin\Test\Docopt.exe --help || exit /b 1
